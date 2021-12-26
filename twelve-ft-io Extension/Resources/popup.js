2f8ff463efbc7beb76851929a8e1dcdcf44b2197
Jason Klein <jklein.64@verizon.net>
2021-12-26 18:32:54 -0500

    Add interactivity to popup.  It works!
    Tested on a medium.com article: https://medium.com/@cyborg.team.nitr/miou-calculation-4875f918f4cb.  The specific API documentation
    is actually available through MDN at https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/

--- twelve-ft-io Extension/Resources/popup.js
+++ twelve-ft-io Extension/Resources/popup.js
@@ -1,1 +1,17 @@
-console.log("Hello World!", browser);
+// document is the rendered extension popup, not the current tab
+const button = document.querySelector(".logo-button")
+
+button.addEventListener("click", async () => {
+    // get the current/active tab
+    const tab = (await browser.tabs.query({ active: true })).pop()
+    
+    // prepend 12ft.io
+    const url = "https://12ft.io/" + tab.url.replace(/https?:\/\//, "")
+    
+    // update the current tab's url
+    await browser.tabs.update({
+        active: true,
+        highlighted: true,
+        url
+    })
+})
